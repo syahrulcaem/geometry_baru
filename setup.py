1db189e2cc3e36c48b1e4ca6cfbc4369d9e81506
syahrulcaem <syahrulromadhonmuhammad@gmail.com>
2023-11-17 15:58:31 +0700

    ...

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url='https://github.com/hanifaslamm/new-mygeometry-hanif',
+    url='https://github.com/syahrulcaem/new_mygeometry14',
     packages=find_packages(),
     license='MIT',
     install_requires=[],
